1f7258f8aff8dff6613ca74358e7fe3f09c1ee77
begalievn <begaliev.info@gmail.com>
2022-11-29 16:07:56 +0600

    Added tests to user.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
-import { JwtService } from '@nestjs/jwt';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
@@ -10,33 +10,33 @@ export class AuthService {
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.userService.findOne({username});
-    if(user && user.password === pass) {
+    const user = await this.userService.findOne({ username });
+    if (user && user.password === pass) {
       const result = {
         userId: user._id.valueOf(),
         username: user.username
-      }
+      };
       return result;
     }
     return null;
   }
 
   async login(user: any) {
-    try{
+    try {
       const payload = { userId: user.userId, username: user.username };
       return {
         access_token: this.jwtService.sign(payload)
-      }
-    }catch (e){
-      console.log(e.message)
+      };
+    } catch (e) {
+      console.log(e.message);
     }
   }
 
   async register(user) {
-    try {
-      return await this.userService.createUser(user);
-    } catch(e) {
-      console.log(e.message);
+    const checkUser = await this.userService.findOne({ username: user.username });
+    if (checkUser) {
+      throw new BadRequestException("User already exists");
     }
+    return await this.userService.createUser(user);
   }
 }
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from "@nestjs/mongoose";
+import { User } from './entities/user.model';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+
+  let mockUserModel = {
+    findOne: jest.fn().mockImplementation((searchQuery) => {
+      return Promise.resolve({
+        name: 'Maria',
+        username: 'maria',
+      })
+    }),
+
+    create: jest.fn().mockImplementation((user) => {
+      return Promise.resolve({
+        name: 'Maria',
+        username: 'maria'
+      })
+    })
+  }
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel
+        }
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = moduleRef.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
+
+  it('should create a user', async () => {
+    const user = {
+      name: 'Maria',
+      username: 'maria',
+      password: 'guess'
+    }
+
+    expect(await userService.createUser(user)).toEqual({
+      username: 'maria',
+      name: 'Maria'
+    })
+  })
 });
